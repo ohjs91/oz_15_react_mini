8c2834501c046caa00499dffd3d0e165baabbb3d
진석 <ozjinseok@gmail.com>
2025-12-04 16:48:20 +0900

    feat : 2-2 API 호출하기

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
+export const API_TOKEN = import.meta.env.VITE_API_TOKEN;
+export const API_KEY = import.meta.env.VITE_API_KEY;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MovieData from '@/data/movieListData.json';
 import MovieCard from '@/components/MovieCard';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -6,11 +6,22 @@ import { Navigation, Pagination, A11y, Zoom } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-const token = import.meta.env.VITE_API_TOKEN;
-const apiKey = import.meta.env.VITE_API_KEY;
+import { API_KEY, API_TOKEN, BASE_URL } from '@/constants';
 const Home = () => {
-  console.log(token);
-  console.log(apiKey);
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_TOKEN}`,
+      },
+    };
+
+    fetch(`${BASE_URL}popular?language=ko`, options)
+      .then((res) => res.json())
+      .then((res) => console.log('TMDB API 결과:', res))
+      .catch((err) => console.error(err));
+  }, []);
   return (
     <>
       {/* 스와이퍼 영역 나중에 컴포넌트 분리하기 */}
