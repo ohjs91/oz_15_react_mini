f1dcc8c7936fc511631100b9beb30aa0748700b7
진석 <ozjinseok@gmail.com>
2025-12-17 01:25:41 +0900

    불필요한 파일 제거

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from '@/context/index';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      {/* <AuthProvider> */}
       <App />
-      {/* </AuthProvider> */}
     </BrowserRouter>
   </QueryClientProvider>,
 );
