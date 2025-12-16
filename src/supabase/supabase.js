4c8cc9ac0fc3993460892180958d51c37541a234
진석 <ozjinseok@gmail.com>
2025-12-17 00:29:44 +0900

    test : supabase test

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -2,5 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_PROJECT_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
-
+console.log('ENV URL:', supabaseUrl);
+console.log('ENV KEY exists:', !!supabaseKey);
 export const supabase = createClient(supabaseUrl, supabaseKey);
